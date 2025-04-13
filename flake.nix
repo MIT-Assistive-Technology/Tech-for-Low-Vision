67279ec45dfa35be63f508a8fcf3ffbb1eb25f20
Ananth Venkatesh <dev.quantum9innovation@gmail.com>
2025-04-13 18:09:45 -0400

    build(nix): add pre-commit checks and apps

--- flake.nix
+++ flake.nix
@@ -4,17 +4,36 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      pre-commit-hooks,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
         python = pkgs.python312;
         pythonPackages = python.pkgs;
-      in {
+      in
+      {
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              nixfmt-rfc-style.enable = true;
+            };
+          };
+        };
+
         devShells.default = pkgs.mkShell {
-          buildInputs = [
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+          buildInputs = self.checks.${system}.pre-commit-check.enabledPackages ++ [
             python
             pythonPackages.trimesh
             pythonPackages.numpy
@@ -22,10 +41,23 @@
             pythonPackages.scipy
             pythonPackages.flask
           ];
-          shellHook = ''
-            echo "Python development environment loaded."
-          '';
         };
+
+        apps = {
+          default =
+            let
+              runner = pkgs.writeScriptBin "run" ''
+                #!${pkgs.bash}/bin/bash
+                ${pythonPackages.flask}/bin/flask run
+              '';
+            in
+            {
+              type = "app";
+              program = "${runner}/bin/run";
+            };
+        };
+
+        formatter = nixpkgs.legacyPackages.${system}.nixfmt-rfc-style;
       }
     );
 }
